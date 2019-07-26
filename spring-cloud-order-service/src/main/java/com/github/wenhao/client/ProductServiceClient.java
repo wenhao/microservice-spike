2527a188dfd21ece22fdbc25491a491488bbdfad
Wen Hao <wenhao@126.com>
2019-07-26 22:03:27 +0800

    [TECH][wenhao] rename to product service.

--- spring-cloud-order-service/src/main/java/com/github/wenhao/client/ProductServiceClient.java
+++ spring-cloud-order-service/src/main/java/com/github/wenhao/client/ProductServiceClient.java
@@ -2,12 +2,13 @@ package com.github.wenhao.client;
 
 import com.github.wenhao.domain.Product;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "SPRING-CLOUD-PRODUCT-SERVICE")
 public interface ProductServiceClient {
 
-    @RequestMapping(value = "products", method = RequestMethod.GET)
-    Product get(Long productId);
+    @RequestMapping(value = "products/{productId}", method = RequestMethod.GET)
+    Product get(@PathVariable Long productId);
 }
