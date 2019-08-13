600ec13a5268ff294fed5c1e61bed5f236764b3d
Wen Hao <wenhao@126.com>
2019-08-13 13:51:55 +0800

    [TECH][wenhao] rename to product service.

--- spring-cloud-product-service/src/main/java/com/github/wenhao/Application.java
+++ spring-cloud-product-service/src/main/java/com/github/wenhao/Application.java
@@ -3,7 +3,9 @@ package com.github.wenhao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 @EnableDiscoveryClient
 public class Application {
